131e075bce3dc125d8d471d6d0559af0f0f6d2d4
drinking <pan49@126.com>
2015-05-01 11:02:28 +0800

    Update Demo

--- Example/DKLightSideScrollView/DKViewController.m
+++ Example/DKLightSideScrollView/DKViewController.m
@@ -9,8 +9,6 @@
 #import "DKViewController.h"
 
 #import <DKLightSideScrollView/DKLightSideScrollView.h>
-//#import "DKLightSideScrollView.h"
-//#import "DKLightSideScrollItemView.h"
 
 @interface DKViewController ()<DKLightSideScrollViewDelegate>
 @property (nonatomic, strong) DKLightSideScrollView *scrollView;
@@ -26,25 +24,36 @@
     
     [self.view addSubview:_scrollView];
     
-    for (int i=0;i<19;i++){
+    for (int i=0;i<5;i++){
         DKLightSideScrollItemView *itemView = [[DKLightSideScrollItemView alloc] initWithFrame:CGRectMake(0, 0, 5*i+10, 50)];
         itemView.backgroundColor = [UIColor redColor];
         [self.scrollView addScrollItemView:itemView];
     }
+
+    UIButton *addButton = [[UIButton alloc] initWithFrame:CGRectMake(0, CGRectGetHeight(self.view.frame)-50, CGRectGetWidth(self.view.frame), 50)];
+    addButton.backgroundColor = [UIColor blackColor];
+    [addButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [addButton setTitle:@"Add" forState:UIControlStateNormal];
+    [addButton addTarget:self action:@selector(addItem) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:addButton];
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
 
+}
+
+- (void)addItem{
+    DKLightSideScrollItemView *itemView = [[DKLightSideScrollItemView alloc] initWithFrame:CGRectMake(0, 0, 30, 50)];
+    itemView.backgroundColor = [UIColor redColor];
+    [self.scrollView addScrollItemView:itemView];
 }
 
 - (void)scrollView:(DKLightSideScrollView *)scrollView didSelectItem:(DKLightSideScrollItemView *)item{
     [self.scrollView removeScrollItemView:item];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
